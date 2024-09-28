d362ce7f05c1293047e7763ba3afc1bf045412fa
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-28 16:03:58 +0600

    add pricing page and adjust styles.css accordingly

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__, template_folder=".")
 def serve_index():
     return render_template("index.html")
 
-
 @app.route("/about")
 def serve_about():
     return render_template("about.html")
+
+@app.route("/pricing")
+def serve_pricing():
+    return render_template("pricing.html")
 
 @app.route("/<path:dummy>")
 def page_not_found(dummy):

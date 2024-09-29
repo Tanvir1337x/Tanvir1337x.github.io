bd0d304ff7a68ecf73b6cd62220204f5439c4942
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-09-29 17:18:15 +0600

    add fancy cursor trail effect on the site

--- static/script.js
+++ static/script.js
@@ -1,1 +1,24 @@
-// Note: JavaScript code (if needed) goes here
+class NeonTrail {
+  constructor() {
+    this.trails = [];
+    document.addEventListener("mousemove", this.createTrail.bind(this));
+  }
+
+  createTrail(e) {
+    const trail = document.createElement("div");
+    trail.className = "trail";
+    document.body.appendChild(trail);
+
+    trail.style.left = `${e.pageX}px`;
+    trail.style.top = `${e.pageY}px`;
+
+    this.trails.push(trail);
+
+    setTimeout(() => {
+      trail.remove();
+      this.trails.shift();
+    }, 700);
+  }
+}
+
+new NeonTrail();
